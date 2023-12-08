db04441b893b4e297d98bf55db1c3e48d03b295f
Hanck Edd <hanckeddison@gmail.com>
2023-12-08 10:22:38 -0800

    window size

--- main.c
+++ main.c
@@ -8,6 +8,23 @@
 #include <stdlib.h> /* Se añadió la inclusión para malloc y free*/
 #include <stdbool.h>
 
+typedef struct _winConfig
+{
+	int screenWidth;
+	int screenHeight;
+	int newWidth;
+	int newHeight;
+	float aspectRatio;
+	float aspect;
+} twinConfig;
+
+typedef twinConfig *winConfig;
+
+
+/*functions*/
+void prepareWin(winConfig windowConfs);
+void fixAspectRatio(winConfig windowConfs);
+
 #ifdef _WIN32
 int wmain(int argc, char *argv[]) {
 #endif 
@@ -17,9 +34,11 @@ int main(int argc, char *argv[]) {
 	/* Hacer declaraciones */
 	SDL_Window *window;
 	SDL_GLContext glContext;
+	winConfig mainWinConfig = malloc(sizeof(twinConfig));
 	GLenum glewError;
-
 	bool quit = false;
+	prepareWin(mainWinConfig);
+
 
 	/* Inicializar SDL*/
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -35,11 +54,11 @@ int main(int argc, char *argv[]) {
 	/* Crear ventana*/
 	window = SDL_CreateWindow
 		(
-			"Hola Mundo SDL2 con OpenGL",
+			"CPONG",
 			SDL_WINDOWPOS_CENTERED,
 			SDL_WINDOWPOS_CENTERED,
-			800, 600,
-			SDL_WINDOW_OPENGL
+			mainWinConfig->screenWidth, mainWinConfig->screenHeight,
+			SDL_WINDOW_OPENGL |SDL_WINDOW_RESIZABLE
 		);
 
 	if (!window) {
@@ -85,6 +104,11 @@ int main(int argc, char *argv[]) {
 						quit = true;
 					}
 					break;
+				case SDL_WINDOWEVENT:
+					if (event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
+					{
+						fixAspectRatio(mainWinConfig);
+					}
 			}
 		}
 
@@ -103,4 +127,38 @@ int main(int argc, char *argv[]) {
 	SDL_Quit();
 
 	return 0;
+}
+
+void prepareWin(winConfig windowConfs)
+{
+	windowConfs->aspectRatio = 20;
+	windowConfs->screenWidth = 320;
+	windowConfs->screenHeight = 180;
+	windowConfs->newWidth = 0;
+	windowConfs->newHeight = 0;
+	windowConfs->aspect = 0;
+}
+
+void fixAspectRatio(winConfig windowConfs)
+{
+	windowConfs->screenWidth = windowConfs->newWidth;
+	windowConfs->screenHeight = windowConfs->newHeight;
+
+	glViewport(0, 0, windowConfs->screenWidth, windowConfs->screenHeight);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if (windowConfs->screenWidth >= windowConfs->screenHeight * windowConfs->aspectRatio)
+	{
+		windowConfs->aspect = (float) windowConfs->screenWidth / (float) windowConfs->screenHeight;
+		glOrtho(-windowConfs->aspect * windowConfs->aspectRatio, windowConfs->aspect * windowConfs->aspectRatio, -1.0, 1.0, -1.0, 1.0);
+	}
+	else
+	{
+		windowConfs->aspect = (float)windowConfs->screenHeight / (float)windowConfs->screenWidth;
+		glOrtho(-1.0, 1.0, -windowConfs->aspect / windowConfs->aspectRatio, windowConfs->aspect / windowConfs->aspectRatio, -1.0, 1.0);
+	}
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	printf("Pantalla redimensionada\n");
 }
